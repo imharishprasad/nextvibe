6c0cad2c0d0030326d53a830dede7a17dd285ce6
imharishprasad <hp.harishprasad@gmail.com>
2025-02-12 22:48:01 +0530

    chore: add meta-data for SEO and link manifest.json

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,22 @@
-export default function Home() {
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "NextVibe",
+  description: "NextVibe is a modern portfolio website.",
+  themeColor: "#1a202c",
+  manifest: "/manifest.json",
+  icons: {
+    icon: "/icons/icon-192x192.png",
+    apple: "/icons/icon-192x192.png",
+  },
+};
+
+export default function HomePage() {
   return (
-    <div ></div>
+    <>
+      <main>
+       
+      </main>
+    </>
   );
 }
