a1d027411206e9066d1f0a0be2ed96231fb1c98e
imharishprasad <hp.harishprasad@gmail.com>
2025-02-12 22:35:51 +0530

    fix: loader visible for every reload. - render loader only for / meaning on home page, not on any other page. - conditionally render the header and footer, to avoid overlapping of loader elements on header and footer. - when loader is shown, don't render the header and footer comp.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import { Geist, Geist_Mono } from "next/font/google";
+import { usePathname } from "next/navigation";
 import "../styles/globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import ScrollToTopButton from "@/components/ScrollToTopButton";
 import Loader from "@/components/Loader";
+import { useState, useEffect } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,22 +24,39 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const pathname = usePathname();
+  const [showContent, setShowContent] = useState(pathname !== "/");
+
+  useEffect(() => {
+    if (pathname === "/") {
+      setShowContent(false);
+    } else {
+      setShowContent(true);
+    }
+  }, [pathname]);
+
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Loader
-          onComplete={() => {}}
-          text="NextVibe"
-          duration={2500}
-          bgColor="bg-black"
-          textColor="text-green-400"
-          effect="wave"
-        />
-        <Header />
-        {children}
-        <Footer />
+        {pathname === "/" && !showContent ? (
+          <Loader
+            onComplete={() => setShowContent(true)}
+            text="NextVibe"
+            duration={2500}
+            bgColor="bg-black"
+            textColor="text-green-400"
+            effect="wave"
+          />
+        ) : (
+          <>
+            <Header />
+            {children}
+            <Footer />
+          </>
+        )}
+
         <ScrollToTopButton />
       </body>
     </html>
