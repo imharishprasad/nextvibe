a97e0ad3358ecaaedcb409e14a703e38a7d0aba4
imharishprasad <hp.harishprasad@gmail.com>
2025-02-12 22:44:16 +0530

    chore: configure PWA, disable for dev mode.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
   /* config options here */
 };
 
 export default nextConfig;
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+});
+
+module.exports = withPWA(nextConfig);
