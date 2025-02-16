00b5fb5b900d92984fa97f637e937602808779c0
imharishprasad <hp.harishprasad@gmail.com>
2025-02-16 23:48:13 +0530

    fix: Now both the light and dark mode works well. - Added darkMode flag class and also update styling. - Added neumorphism design, will consume in card design in future enhancement.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: 'class',
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -12,7 +13,25 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      //card
+      boxShadow: {
+        neumorphism:
+          "8px 8px 16px #bebebe, -8px -8px 16px #ffffff",
+      },
+      //timeline
+      animation: {
+        'fade-in': 'fadeIn 1s ease-in-out',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-neumorphism'),
+    require("@tailwindcss/typography")
+  ],
 } satisfies Config;
