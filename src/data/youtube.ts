db5c9772dd4b64b6ca22b190afdbde6efcb3dee8
imharishprasad <hp.harishprasad@gmail.com>
2025-03-09 18:31:52 +0530

    chore: Manually populate the youtube meta data for loading content fow now. - Will fetch from youtube api later based on id.

---
+++ src/data/youtube.ts
@@ -0,0 +1,52 @@
+export const videos = [
+    {
+      id: "1", 
+      youtubeId: "M8WiW5CfzBs",
+      title: "Test",
+      description: "Experimental Video used to test card feature.",
+      thumbnail: "/images/default-blog-thumbnail.webp",
+    },
+    {
+      id: "2",
+      youtubeId: "QHDI6WlQ4O8",
+      title: "How To Read Data From Computer Memory (RAM) | Programming | Computer Memory Hacks",
+      description: "In this video, I will show you how to read Data directly from RAM (Random Access Memory). Everyone knows how to read data from a Drive such as HDD(Hard Disk Drive) or SSD(Solid State Drive) but the challenge lies in reading data from RAM, as it contains the most relevant information and the end-user doesn’t have direct access to RAM Data.",
+      thumbnail: "",
+    },
+    {
+      id: "3",
+      youtubeId: "bJ7JKsvaWCI",
+      title: "Introduction To Terminal | SHELL | COMMAND LINE | Terminal Made easy in 2022",
+      description: "In this video, I will try to Demystify some of the confusing Terminology related to Character/text Based User Interfaces such as Terminal, Shell, Command-Line, Console, Bash, etc.",
+      thumbnail: "",
+    },
+    {
+      id: "4",
+      youtubeId: "uBdpHUZNN3I",
+      title: "Logging into Local System and the Remote System using Terminal and SSH Protocol",
+      description: "In this video, I will Talk about the different ways to log into the local system and the remote system using terminal and SSH Protocol.",
+      thumbnail: "",
+    },
+    {
+        id: "5", 
+        youtubeId: "M8WiW5CfzBs",
+        title: "Getting Started With Linux For Absolute Beginners 2022.",
+        description: "This Linux Beginner tutorial will demonstrate to you how to get started using the Ubuntu Linux operating system (OS). If you’ve never used a Linux-based operating system, Ubuntu is a great entry-level Linux distribution that’s user-friendly for beginners, especially for those of you that have been using macOS or Windows. In this beginner’s tutorial, I will show the basics to help you get started using Ubuntu.",
+        thumbnail: "",
+      },
+      {
+        id: "6", 
+        youtubeId: "K36cPJtUkqA",
+        title: "9 Useful Linux Terminal Tips, Tricks and Cool Commands to Boost your Productivity in 2022!",
+        description: "Here in this video, I have demonstrated some powerful Linux Terminal Hacks using some cool commands in RedHat Enterprise Linux(RHEL8) to boost your productivity when working on any Linux based Operating system such as Linux Mint, Debian, Ubuntu, Fedora, CentOS, KaliLinux, Parrot, Arch Linux, etc.",
+        thumbnail: "",
+      }
+      ,
+      {
+        id: "7", 
+        youtubeId: "K36cPJtUkqA",
+        title: "9 Useful Linux Terminal Tips, Tricks and Cool Commands to Boost your Productivity in 2022!",
+        description: "Here in this video, I have demonstrated some powerful Linux Terminal Hacks using some cool commands in RedHat Enterprise Linux(RHEL8) to boost your productivity when working on any Linux based Operating system such as Linux Mint, Debian, Ubuntu, Fedora, CentOS, KaliLinux, Parrot, Arch Linux, etc.",
+        thumbnail: "",
+      }
+  ];
